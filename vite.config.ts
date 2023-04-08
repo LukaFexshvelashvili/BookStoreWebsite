92da6a75b6f31a4bba370f75ee81b5e3fd3bc2e3
ModernDesignGe <moderndesignge@gmail.com>
2023-04-08 17:12:11 +0400

    Config Changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/BookStoreWebsite/",
   plugins: [react()],
   server: {
     host: true,
